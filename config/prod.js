6e829a1954a420cd5e5d0d90f153f96d61db3314
bkglobal <khatribilal5@gmail.com>
2020-07-07 15:48:55 +0500

    modified app

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,23 @@
-require('dotenv').config()
-module.exports = {
-    mongoURI:process.env.MONGO_URI,
-    Server:process.env.Server,
-    mailthrough:process.env.mailthrough,
+// require('dotenv').config()
+// module.exports = {
+//     mongoURI:process.env.MONGO_URI,
+//     Server:process.env.Server,
+//     mailthrough:process.env.mailthrough,
     
+// }
+
+module.exports = {
+    mongoURI: 'mongodb+srv://breeder:GtmwECDdd3sL26N3@breederdb-vruiv.mongodb.net/breeder?retryWrites=true&w=majority'
+    //   mongoURI:'mongodb://localhost:27017/breeder_official'
+    , //Server: 'http://localhost:5000',
+    Server: 'http://localhost:3000',
+    mailthrough: 'admin@breeder.com',
+
+    //stripe
+    stripe_publishable:"pk_test_4UM0NJail2U84LTdxbWH90GH00BcqCrNYn",
+    stripe_private:"sk_test_Cozb0IU8FFmiHpepGUVqQCUM00gNg0NJRk",
+
+    //paypal
+    paypalId:"",
+    paypalSecret:"",
 }
