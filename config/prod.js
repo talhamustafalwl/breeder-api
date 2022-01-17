c8f681d20ad9d2cb0abf73636b422f84cf856ad7
farrukhazeem <farrukh90@hotmail.com>
2022-01-17 10:40:42 +0500

    for staging server env

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,9 @@
-require('dotenv').config();
-
+require("dotenv").config();
 
 module.exports = {
-  mongoURI: 'mongodb+srv://breeder:GtmwECDdd3sL26N3@breederdb-vruiv.mongodb.net/breeder?retryWrites=true&w=majority',
+  mongoURI:
+    "mongodb+srv://breeder:GtmwECDdd3sL26N3@breederdb-vruiv.mongodb.net/breeder?retryWrites=true&w=majority",
   //   mongoURI:'mongodb://localhost:27017/breeder_official'
-
 
   // Server: 'http://192.168.137.36:5000/api',
   // webServer: 'http://192.168.137.36:5000/api',
@@ -14,7 +13,6 @@ module.exports = {
   // imageURL: 'http://192.168.137.36:5000/api/uploads/images/form/',
   // baseDocumentURL: 'http://192.168.137.36:5000/api/uploads/documents/',
 
-
   // Server: 'http://192.168.100.29:5000/api',
   // webServer: 'http://192.168.100.29:5000/api',
   // baseAPIUrl: 'http://192.168.100.29:5000/api',
@@ -22,7 +20,6 @@ module.exports = {
   // serverURL: 'http://192.168.100.29:5000/api',
   // imageURL: 'http://192.168.100.29:5000/api/uploads/images/form/',
   // baseDocumentURL: 'http://192.168.100.29:5000/api/uploads/documents/',
-
 
   // Server: 'http://localhost:5000/api',
   // webServer: 'http://localhost:3000',
@@ -40,35 +37,26 @@ module.exports = {
   // imageURL: 'https://breeder-api.herokuapp.com/uploads/images/form/',
   // baseDocumentURL: 'https://breeder-api.herokuapp.com/uploads/documents/',
 
+  Server: "http://3.140.247.226/api",
+  webServer: "http://3.140.247.226",
+  baseAPIUrl: "http://3.140.247.226/api/",
+  baseImageURL: "http://3.140.247.226/api/uploads/images/",
+  serverURL: "http://3.140.247.226/api",
+  imageURL: "http://3.140.247.226/api/uploads/images/form/",
+  baseDocumentURL: "http://3.140.247.226/api/uploads/documents/",
 
+  // Server: 'https://logly.us/api',
+  // webServer: 'https://logly.us',
+  // baseAPIUrl: 'https://logly.us/api/',
+  // baseImageURL: 'https://logly.us/api/uploads/images/',
+  // serverURL: 'https://logly.us/api',
+  // imageURL: 'https://logly.us/api/uploads/images/form/',
+  // baseDocumentURL: 'https://logly.us/api/uploads/documents/',
+  // basecharityDoc: 'https://logly.us/api/uploads/charityDoc/',
 
-
-  // Server: 'http://3.21.129.23/api',
-  // webServer: 'http://3.21.129.23',
-  // baseAPIUrl: 'http://3.21.129.23/api/',
-  // baseImageURL: 'http://3.21.129.23/api/uploads/images/',
-  // serverURL: 'http://3.21.129.23/api',
-  // imageURL: 'http://3.21.129.23/api/uploads/images/form/',
-  // baseDocumentURL: 'http://3.21.129.23/api/uploads/documents/',
-
-
-  Server: 'https://logly.us/api',
-  webServer: 'https://logly.us',
-  baseAPIUrl: 'https://logly.us/api/',
-  baseImageURL: 'https://logly.us/api/uploads/images/',
-  serverURL: 'https://logly.us/api',
-  imageURL: 'https://logly.us/api/uploads/images/form/',
-  baseDocumentURL: 'https://logly.us/api/uploads/documents/',
-  basecharityDoc: 'https://logly.us/api/uploads/charityDoc/',
-
-
-
-
-
-  mailthrough: 'hello@logly.us',
-  mailFeedback: 'hello@logly.us',
+  mailthrough: "hello@logly.us",
+  mailFeedback: "hello@logly.us",
   sendgridAPIKey: "",
-
 
   //stripe client
   //(testing)
@@ -87,14 +75,10 @@ module.exports = {
 
   paypalSecret: "",
 
-
-
   //Server: 'https://breeder-api.herokuapp.com',
   // Server: 'http://localhost:3000',
   // Server: 'https://breeder-dev.herokuapp.com',
   // baseAPIUrl: 'http://localhost:3000/',
 
   sendgridAPIKey: process.env.SENDGRID_API_KEY,
-
-
-}
+};
