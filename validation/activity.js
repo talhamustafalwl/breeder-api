784aca7d0d426f9d1994ca6534f8c6465424a426
farrukhazeem <farrukh90@hotmail.com>
2022-02-14 14:51:08 +0500

    remove categoryType

--- validation/activity.js
+++ validation/activity.js
@@ -4,14 +4,14 @@ const isEmpty = require("is-empty");
 function validateActivity(data) {
   let errors = {};
 
-  data.categoryType = data.categoryType ? data.categoryType : "";
+  //   data.categoryType = data.categoryType ? data.categoryType : "";
   // data.description = data.description ? data.description : '';
   data.categoryName = data.categoryName ? data.categoryName : "";
 
   console.log(data);
-  if (Validator.isEmpty(data.categoryType)) {
-    errors.categoryType = "categoryType is required";
-  }
+  //   if (Validator.isEmpty(data.categoryType)) {
+  //     errors.categoryType = "categoryType is required";
+  //   }
   //   if (Validator.isEmpty(data.description)) {
   //     errors.description = "Description is required";
   //   }
